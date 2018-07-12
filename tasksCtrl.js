cb0e70bdf6eaa46fa7b1dbd0212f5e9577c1f573
arielaleco <arielaleco@gmail.com>
2018-07-12 22:16:43 +0300

    filter is working

--- tasksCtrl.js
+++ tasksCtrl.js
@@ -1,36 +1,55 @@
 app.controller("tasksCtrl", function ($scope, tasksServices) {
 
     $scope.tasksArr = [];
-    $scope.needTodo ="need to do";
-    
+    $scope.needTodo = "need to do";
 
-$scope.PushMe = function (task) {
-    tasksServices.addTask(task);
-    $scope.tasksArr = tasksArr;
-   
-}
 
-$scope.DeleteTheTask = function (task) {
-    tasksServices.deleteTask(task);
-    $scope.tasksArr = tasksArr;
-   
-}
+    $scope.PushMe = function (task) {
+        tasksServices.addTask(task);
+        $scope.tasksArr = tasksArr;
 
-$scope.filter= {
-    all: false,
-    complete: false,
-    active: false
-};
-
-$scope.filterTasks = function(task) {
-
-    console.log($scope.filter);
-    if (task && (task.taskDone) && ($scope.filter.complete)){
-      return false;
-    } else {
-      return true;
     }
-    // return (car.brand.includes($scope.query) || car.model.includes($scope.query))
-  }
 
+    $scope.DeleteTheTask = function (task) {
+        tasksServices.deleteTask(task);
+        $scope.tasksArr = tasksArr;
+
+    }
+    $scope.filter = 1;
+    $scope.changefilter = function (setfilter) {
+        $scope.filter = setfilter;
+
+        console.log($scope.filter);
+        // $scope.selected = "active";
+
+        // console.log($scope.my);
+    }
+
+    $scope.filterTasks = function (task) {
+
+        console.log(task);
+        if ($scope.filter == 1) //all 
+        {
+            console.log("in all");
+            return true;
+        }
+        else
+            if ($scope.filter == 2) //Active 
+            {
+                console.log("in active");
+                if (task.taskDone) { return false; }
+                else { return true; }
+
+
+            }
+        if ($scope.filter == 3) //Complete 
+        {
+            console.log("Complete");
+            if (task.taskDone) { return true; }
+            else { return false; }
+        }
+                
+    }
+
+    
 });
